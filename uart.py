9d099318e98d6fa4bc2fce4ca35dc6837859fb46
Lenny Truong <lenny@cs.stanford.edu>
2017-09-12 20:48:51 -0700

    Update uart implementation

--- uart.py
+++ uart.py
@@ -2,7 +2,7 @@ from coroutine import coroutine
 from magma.bitutils import int2seq
 
 
-def tx(data):
+def uart_transmit(data):
     yield 0
     yield from data
     # for d in data:
@@ -10,8 +10,7 @@ def tx(data):
     yield 1
 
 
-@coroutine
-def uart_rx():
+def uart_receive():
     data = [0 for _ in range(8)]
     while True:
         RX = yield
@@ -20,18 +19,13 @@ def uart_rx():
                 data[i] = yield
             RX = yield
             if RX == 1:
-                yield data
-
+                return data
 
 @coroutine
 def main():
-    rx = uart_rx()
     while True:
-        value = None
-        while value is None:
-            RX = yield
-            value = rx.send(RX)
-        yield from tx(value)
+        value = yield from uart_receive()
+        yield from uart_transmit(value)
 
 main_ = main()
 
@@ -39,9 +33,9 @@ data = int2seq(0xBE)
 print("Expected: 0 " + " ".join(str(x) for x in data) + " 1")
 print("Actual  : ", end="")
 for RX in [0] + data + [1]:
-    TX = main_.send(RX)
-print(TX, end=" ")  # First bit is sent on last bit
+    transmit_bit = main_.send(RX)
+print(transmit_bit, end=" ")  # First bit is sent on last bit
 
 for _ in range(len(data) + 1):
-    TX = main_.send(None)
-    print(TX, end=" ")
+    transmit_bit = main_.send(None)
+    print(transmit_bit, end=" ")
