863b8d92ac3b769789057634c3c86feb71b66970
Lenny Truong <lenny@cs.stanford.edu>
2017-09-12 20:45:02 -0700

    Remove old coroutine code

--- coroutine.py
+++ coroutine.py
@@ -1,8 +1,3 @@
-# Adapted for Python 3 from http://www.dabeaz.com/coroutines/coroutine.py
-# coroutine.py
-#
-# A decorator function that takes care of starting a coroutine
-# automatically on call.
 import types
 
 class Coroutine():
@@ -19,24 +14,6 @@ class Coroutine():
 
 def coroutine(func):
     def start(*args,**kwargs):
-        # cr = func(*args,**kwargs)
-        # next(cr)
         cr = types.coroutine(func)(*args,**kwargs)
         return Coroutine(cr)
     return start
-
-# Example use
-if __name__ == '__main__':
-    @coroutine
-    def grep(pattern):
-        print("Looking for %s" % pattern)
-        while True:
-            line = (yield)
-            if pattern in line:
-                print(line)
-
-    g = grep("python")
-    # Notice how you don't need a next() call here
-    g.send("Yeah, but no, but yeah, but no")
-    g.send("A series of tubes")
-    g.send("python generators rock!")
