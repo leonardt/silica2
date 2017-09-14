98a0c47796d50c8b9829a391b28ca3f4090d34ee
Lenny Truong <lenny@cs.stanford.edu>
2017-09-14 16:54:26 -0700

    Use BitVector in fifo definition

--- fifo.py
+++ fifo.py
@@ -1,34 +1,33 @@
 from coroutine import coroutine
 from magma.bitutils import int2seq, seq2int
+from magma.bit_vector import BitVector
 import numpy as np
 
 
 class Fifo:
-    def __init__(self, height=2, width=None):
-        self.height = height
+    def __init__(self, depth=2, width=None):
+        self.depth = depth
         self.width = width
         if width is None:
-            self._values = np.ndarray(height, np.bool)
+            self._values = np.ndarray(depth, np.bool)
         else:
-            self._values = np.ndarray((height, width), np.bool)
-        self._curr_write_idx = 1
-        self._curr_read_idx = 0
+            self._values = np.ndarray((depth, width), np.bool)
+        addr_width = (depth - 1).bit_length()
+        self._curr_write_idx = BitVector(1, num_bits=addr_width)
+        self._curr_read_idx = BitVector(0, num_bits=addr_width)
 
     def push(self, value):
         if self._curr_write_idx == self._curr_read_idx:
             return
-        self._values[self._curr_write_idx] = value
-        self._curr_write_idx += 1
-        if self._curr_write_idx == self.height:
-            self._curr_write_idx = 0
+        self._values[self._curr_write_idx.as_int()] = value
+        self._curr_write_idx += BitVector(1, num_bits=len(self._curr_write_idx))
 
     def pop(self):
-        if self._curr_read_idx == self._curr_write_idx - 1:
+        if self._curr_read_idx == \
+                self._curr_write_idx - BitVector(1, num_bits=len(self._curr_read_idx)):
             return None
-        self._curr_read_idx += 1
-        if self._curr_read_idx == self.height:
-            self._curr_read_idx = 0
-        value = self._values[self._curr_read_idx]
+        self._curr_read_idx += BitVector(1, num_bits=len(self._curr_read_idx))
+        value = self._values[self._curr_read_idx.as_int()]
         return value
 
 
